d0b2375118c43d21d09bc8e8e915c8a3227f5fd7
dhmm <dhmm@outlook.com>
2022-07-28 15:12:20 +0300

    3 for loop

---
+++ 3_for_loops.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int
+	for i = 1; i <= 10; i++ {
+		fmt.Println(i)
+	}
+
+	//Loop with temporary variable
+	for a := 1; a <= 10; a++ {
+		fmt.Println(a)
+	}
+
+}
